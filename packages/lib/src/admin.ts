400dd0db07cd2f6d1fb41fac9816c6f729a04c72
IT Lackey <itlackey@outlook.com>
2026-02-20 01:49:03 -0600

    remove leftover admin src shim to reduce merge conflicts

--- packages/lib/src/admin.ts
+++
@@ -1,6 +0,0 @@
-export * from "../admin/compose-runner.ts";
-export * from "../admin/impact-plan.ts";
-export * from "../admin/stack-apply-engine.ts";
-export * from "../admin/stack-generator.ts";
-export * from "../admin/stack-manager.ts";
-export * from "../admin/stack-spec.ts";
